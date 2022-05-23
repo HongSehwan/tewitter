7705410a0af5248dd105c2e3f9a59b22048bd725
Hong <gg9297@gmail.com>
2022-05-24 07:52:20 +0900

    Add Join/Login/LogOut func

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,50 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { authService } from "fbase";
 
 const Auth = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [newAccount, setNewAccount] = useState(false)
-  const onSubmit = async (event) => {
-    event.preventDefault()
-    try {
-      let data
-      if (newAccount) {
-        data = await authService.createUserWithEmailAndPassword(email, password)
-      } else {
-        data = await authService.signInWithEmailAndPassword(email, password)
-      }
-      console.log(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={onChange}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          value={password}
-          onChange={onChange}
-        />
-        <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
-      </form>
-      <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
-      </div>
-    </div>
-  )
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(false);
+    const [error, setError] = useState("");
+    const onChange = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+            setError(error.message);
+        }
+    };
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
+                <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+                {error}
+            </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 
-export default Auth
+export default Auth;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,20 @@
-import React from 'react'
+import React from "react";
+import { authService } from "fbase";
 
-const Home = () => <span>Home</span>
+const Home = () => {
+    const SignOut = () => {
+        if (authService.currentUser) {
+            authService.signOut();
+        }
+    };
+    return (
+        <div>
+            <span>Home</span>
+            <div>
+                <button onClick={SignOut}>Sign Out</button>
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
