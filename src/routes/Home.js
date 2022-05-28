79143298555692094fba1cea7784c19c78ac149c
Hong <gg9297@gmail.com>
2022-05-28 23:17:25 +0900

    Fix Logout data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,19 +2,7 @@ import React from "react";
 import { authService } from "fbase";
 
 const Home = () => {
-    const SignOut = () => {
-        if (authService.currentUser) {
-            authService.signOut();
-        }
-    };
-    return (
-        <div>
-            <span>Home</span>
-            <div>
-                <button onClick={SignOut}>Sign Out</button>
-            </div>
-        </div>
-    );
+    return <span>Home</span>;
 };
 
 export default Home;
