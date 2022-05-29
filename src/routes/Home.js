3d90c66644b864e61dd7d48adca1e720cf3235ea
Hong <gg9297@gmail.com>
2022-05-29 22:19:01 +0900

    Fix button text

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
         <div>
             <form onSubmit={onSubmit}>
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
-                <input type="submit" value="Nweet" />
+                <input type="submit" value="teweet" />
             </form>
         </div>
     );
