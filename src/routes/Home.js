17bbfda1a23195f4bb8da1edf9448e0ca0cddd90
Hong <gg9297@gmail.com>
2022-05-29 22:17:00 +0900

    Add Home data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useState } from "react";
 
 const Home = () => {
-    return <span>Home</span>;
+    const [nweet, setNweet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNweet(value);
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    );
 };
 
 export default Home;
